757f52b34664ef506b2f27195331afa6592959eb
Amen Hany <amen.hany.2810@gmail.com>
2026-03-03 22:13:49 +0200

    Fix: BGM playing early by secret

--- src/app/projects/_components/ProjectsClient.tsx
+++ src/app/projects/_components/ProjectsClient.tsx
@@ -68,7 +68,7 @@ export default function ProjectsClient() {
                     }
             }
          />
-         <Secrets cleanUp={playBGM} />
+         <Secrets cleanUp={showProjects ? playBGM : () => {}} />
       </>
    );
 }
