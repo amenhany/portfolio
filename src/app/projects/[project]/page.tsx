1b020c8c3afa4124dd5cfc242772a0d7902026e8
Amen Hany <amen.hany.2810@gmail.com>
2026-03-10 17:31:20 +0200

    Add: 404 route handling

--- src/app/projects/[project]/page.tsx
+++ src/app/projects/[project]/page.tsx
@@ -1,7 +1,8 @@
-import { ProjectInfo } from '@/data/projects';
+import { type Project, type ProjectInfo, PROJECTS } from '@/data/projects';
 import BackButton from './_components/BackButton';
 import ProjectDescription from './_components/ProjectDescription';
 import ProjectVideo from './_components/ProjectVideo';
+import { redirect } from 'next/navigation';
 
 export default async function Project({
    params,
@@ -9,6 +10,8 @@ export default async function Project({
    params: Promise<{ project: string }>;
 }) {
    const { project } = await params;
+   if (!PROJECTS.includes(project as Project)) redirect('/projects?dialogue=unknown');
+
    const info = (await import(`@/assets/projects/${project}.json`))
       .default as ProjectInfo;
 
